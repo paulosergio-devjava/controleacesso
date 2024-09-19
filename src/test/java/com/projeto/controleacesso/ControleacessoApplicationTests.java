850063460e330e2859aa7e6d1a31dd3266952a3b
Paulo <pssj@cesar.school>
2024-09-19 00:16:08 -0400

    primeiro commit

---
+++ src/test/java/com/projeto/controleacesso/ControleacessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.controleacesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleacessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
